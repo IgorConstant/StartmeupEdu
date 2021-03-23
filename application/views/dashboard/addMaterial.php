2f45e07114e9490351f3426463664ba8eb1a737c
Igor <igorhenriqueconstant@gmail.com>
2021-03-23 14:10:49 -0300

    Inclusão da ErrorArea

--- application/views/dashboard/addMaterial.php
+++ application/views/dashboard/addMaterial.php
@@ -3,14 +3,14 @@
         <h1 class="h2"><?php echo $titulo_pagina ?></h1>
     </div>
 
-    <!-- <section id="error-area">
+    <section id="error-area">
         <div class="row">
             <div class="col-12 col-sm-12">
                 <?= validation_errors('<div class="alert alert-danger" role="alert">', '</div>') ?>
                 <?= $this->session->userdata('msg'); ?>
             </div>
         </div>
-    </section> -->
+    </section>
 
     <div class="row">
         <div class="col-12 col-sm-12 mb-4">
